dc251790bb1f9007c8cb88fa5b49d5ed424d6811
Rimsha-Urooj <rimshaurooj56@gmail.com>
2021-09-03 15:02:10 +0500

    using stateful lists

--- src/App.js
+++ src/App.js
@@ -1,43 +1,48 @@
 import './App.css';
 import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
+import React, { useState } from 'react';
 
+
+// using stateful lists
+const DUMMY_EXPENSES = [
+  {
+    id:'e1',
+    title: 'Drawing Paper',
+    amount: 94.12,
+    date : new Date(2020, 7, 14),
+  },
+  {
+    id:'e2',
+    title: 'Car Insurance',
+    amount: 294.57,
+    date : new Date(2021, 8, 24),
+  },
+  {
+    id:'e3',
+    title: 'New TV',
+    amount: 799.49,
+    date : new Date(2020, 2, 12),
+  },
+  {
+    id:'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date : new Date(2021, 5, 27),
+  },
+]
 
 function App() {
 
-  const expenses = [
-    {
-      id:'e1',
-      title: 'Drawing Paper',
-      amount: 94.12,
-      date : new Date(2020, 7, 14),
-    },
-    {
-      id:'e2',
-      title: 'Car Insurance',
-      amount: 294.57,
-      date : new Date(2021, 8, 24),
-    },
-    {
-      id:'e3',
-      title: 'New TV',
-      amount: 799.49,
-      date : new Date(2020, 2, 12),
-    },
-    {
-      id:'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date : new Date(2021, 5, 27),
-    },
-  ]
+  // using useState Hook and initiazlizing it with dummy expenses
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
   
   // for bottom to up communication
+  // using previous state to add up in the new state
   const addExpenseHandler = (expense) => {
-    console.log("in App.js");
-    console.log(expenses);
-    console.log("data from child");
-    console.log(expense);
+    setExpenses((prevExpenses) =>{
+      return [expense, ...prevExpenses];
+    });
   }
   return (
     <div className="App">
