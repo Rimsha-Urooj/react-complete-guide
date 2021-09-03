c35569732f86bf7913b4ab306e5e12870ca538e9
Rimsha-Urooj <rimshaurooj56@gmail.com>
2021-09-03 10:45:14 +0500

    updating state that depends on previous state

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -4,54 +4,58 @@ import './ExpenseForm.css'
 function ExpenseForm() {
 
     // Multiple use of States
-    // const [enteredTitle , setEnteredTitle] = useState('');
-    // const [enteredAmount, setEnteredAmount] = useState('');
-    // const [eneteredDate, setEnteredDate] = useState('');
+    const [enteredTitle , setEnteredTitle] = useState('');
+    const [enteredAmount, setEnteredAmount] = useState('');
+    const [eneteredDate, setEnteredDate] = useState('');
 
     // Using one state
-   const [userInput, setUserInput] useState({
-        enteredTitle: '',
-        enteredAmount: '',
-        eneteredDate:'',
-    })
+//    const [userInput, setUserInput] = useState({
+//         enteredTitle: '',
+//         enteredAmount: '',
+//         eneteredDate:'',
+//     })
 
     const titleChangeHandler = (event) => {
         // when use Multiple useState
-        // setEnteredTitle(event.target.value);
+        setEnteredTitle(event.target.value);
 
         // when use single useState function for all inputs
-        setUserInput({
-            // use of spread operator
-            ...userInput,
-            enteredTitle: event.target.value,
-        })
+        // setUserInput({
+        //     // use of spread operator
+        //     ...userInput,
+        //     enteredTitle: event.target.value,
+        // })
 
-        console.log(event.target.value);
+        // updating state that depends on previous state
+        // setUserInput((prevState)=>{
+        //     return { ...prevState, enteredTitle: event.target.value}
+        // });
+        // console.log(event.target.value);
     }
 
     const amountChangeHandler = (event) => {
         // when use Multiple useState
-        // setEnteredAmount(event.target.value);
+        setEnteredAmount(event.target.value);
 
-        // when use single useState function for all inputs
-        setUserInput({
-            // use of spread operator
-            ...userInput,
-            enteredAmount: event.target.value,
-        })
+        // // when use single useState function for all inputs
+        // setUserInput({
+        //     // use of spread operator
+        //     ...userInput,
+        //     enteredAmount: event.target.value,
+        // })
         console.log(event.target.value);
     }
 
     const dateChangeHandler = (event) =>{
         // when use Multiple useState
-        // setEnteredDate(event.target.value);
+        setEnteredDate(event.target.value);
 
         // when use single useState function for all inputs
-        setUserInput({
-            // use of spread operator
-            ...userInput,
-            eneteredDate: event.target.value,
-        })
+        // setUserInput({
+        //     // use of spread operator
+        //     ...userInput,
+        //     eneteredDate: event.target.value,
+        // })
         console.log(event.target.value);
     }
 
